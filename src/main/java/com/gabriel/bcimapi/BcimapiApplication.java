da68654fcb52bd34d8da9be9b07efec226790545
GabrielSalvadorCardoso <gabrielcardoso95@gmail.com>
2023-09-07 00:38:59 -0300

    Deploy configurations

--- src/main/java/com/gabriel/bcimapi/BcimapiApplication.java
+++ src/main/java/com/gabriel/bcimapi/BcimapiApplication.java
@@ -2,12 +2,13 @@ package com.gabriel.bcimapi;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.Bean;
 
 import com.bedatadriven.jackson.datatype.jts.JtsModule;
 
 @SpringBootApplication
-public class BcimapiApplication {
+public class BcimapiApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		//System.setProperty("spring.config.name", "application-dev");
